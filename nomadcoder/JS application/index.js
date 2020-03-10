05cbefd3055bb2789a4a892fced8b3bdabbfed42
JByeongCheon <aglbts88@gmail.com>
2020-03-10 13:52:32 +0900

    net

--- nomadcoder/JS application/index.js
+++ nomadcoder/JS application/index.js
@@ -1,1 +1,10 @@
-alert('Im Working. Im Js. Im Beautiful. Im worth it');
+
+function handleResize(event){
+    console.log(handleResize)
+}
+window.addEventListener("resize",handleResize) //이벤트를 대기하다가 발생하면 handle함수 실행
+
+function handleClick(){ //클릭하면 색 바뀜
+    title.style.color='red';
+}
+title.addEventListener('click',handleClick);
